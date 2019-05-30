8d32f645e062829d318cb6c9e84991ea1189918e
Valoryan <eo824@nyu.edu>
2019-05-29 20:39:48 -0700

    Fixed common bugs

--- DiscoTube/DiscoTube.py
+++ DiscoTube/DiscoTube.py
@@ -5,7 +5,7 @@ client = discord.Client
 @client.event
 async def on_ready():
     print("GET READY TO DISCO)
-    await client.change_presence(game=discord.Game(name = "Disco Dancing"))
+    await client.change_presence(activity = discord.Game(name = "Disco Dancing"))
 
 @client.event
 async def on_message(message):
@@ -14,4 +14,4 @@ async def on_message(message):
     if message.content == "rickroll" or message.content == "Rickroll":
           await client.send_message(message.channel, "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
-client.run(NTgzNDk1NDQ5OTY1NzU2NDIy.XO9M2g.fhtnmfW6UCXiurOoFGuc-Kwo2xs)
+client.run("NTgzNDk1NDQ5OTY1NzU2NDIy.XO9M2g.fhtnmfW6UCXiurOoFGuc-Kwo2xs")

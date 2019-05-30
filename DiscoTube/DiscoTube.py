f7678fb3c120dfb83f74b6dda83eaeb72ddef7df
Valoryan <eo824@nyu.edu>
2019-05-29 20:34:25 -0700

    rickroll bot

--- DiscoTube/DiscoTube.py
+++ DiscoTube/DiscoTube.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import discord
+
+client = discord.Client
+
+@client.event
+async def on_ready():
+    print("GET READY TO DISCO)
+    await client.change_presence(game=discord.Game(name = "Disco Dancing"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+          return
+    if message.content == "rickroll" or message.content == "Rickroll":
+          await client.send_message(message.channel, "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+client.run(NTgzNDk1NDQ5OTY1NzU2NDIy.XO9M2g.fhtnmfW6UCXiurOoFGuc-Kwo2xs)

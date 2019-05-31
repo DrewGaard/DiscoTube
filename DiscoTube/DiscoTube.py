7308c7c5c34ea90a63415806c456ea5aef119cca
Drew Overgaard <d_overgaard1@u.pacific.edu>
2019-05-30 19:57:45 -0700

    Basic youtube parsing with BeautifulSoup

--- DiscoTube/DiscoTube.py
+++ DiscoTube/DiscoTube.py
@@ -1,11 +1,17 @@
 import discord
+from bs4 import BeautifulSoup as bs
+import requests
 
 client = discord.Client()
+
+base = "https://www.youtube.com/results?search_query="
 
 @client.event
 async def on_ready():
     print("GET READY TO DISCO")
     await client.change_presence(activity = discord.Game(name = "Disco Dancing"))
+
+
 
 
 @client.event
@@ -15,6 +21,15 @@ async def on_message(message):
           return
     if message.content == "rickroll":
           await message.channel.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    r = requests.get(base+message.content)
+    page = r.text
+    soup = bs(page, 'html.parser')
+    vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+    videolist=[]
+    for v in vids:
+        tmp = 'https://www.youtube.com' + v['href']
+        videolist.append(tmp)
+    await message.channel.send(videolist[0])
 
 
 client.run("NTgzNDk1NDQ5OTY1NzU2NDIy.XO9M2g.fhtnmfW6UCXiurOoFGuc-Kwo2xs")
